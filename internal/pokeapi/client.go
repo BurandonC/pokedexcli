00ac1b056abac3111547c6fdf3069df4628d07c9
BurandonC <brandonanchau@gmail.com>
2025-05-05 17:03:16 -0700

    implemented caching

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,15 +3,19 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/BurandonC/pokedexcli/internal/pokecache"
 )
 
 // Client
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
